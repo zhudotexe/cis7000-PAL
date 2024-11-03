2a736512d876f3f1ce9fb8fcaeaf803d888dabe3
Andrew Zhu <me@andrew-zhu.com>
2024-11-03 14:58:06 -0500

    fix: make the startup sessions actually work

--- pal-server.py
+++ pal-server.py
@@ -11,39 +11,43 @@ from redel import ReDel
 from redel.server import VizServer
 
 engine = OpenAIEngine(model="gpt-4o", temperature=0.8, top_p=0.95)
-server = VizServer(engine=engine)
 
-# NEIL STARTING PROMPTS
-nicki_system = (Path(__file__).parent / "pal_prompts/nicki_system.txt").read_text().strip()
-nicki_info = (Path(__file__).parent / "pal_prompts/nicki_info.md").read_text().strip()
-nicki = ReDel(
-    engine=engine,
-    system_prompt=nicki_system,
-    title="Nicki Martin, 46 F",
-    extra={"patient_info": nicki_info},  # see redel/utils.py for FrontendExtra
-)
-server.append_new_redel(nicki)
 
-aiden_system = (Path(__file__).parent / "pal_prompts/aiden_system.txt").read_text().strip()
-aiden_info = (Path(__file__).parent / "pal_prompts/aiden_info.md").read_text().strip()
-aiden = ReDel(
-    engine=engine,
-    system_prompt=aiden_system,
-    title="Aiden Brown, 37 M",
-    extra={"patient_info": aiden_info},
-)
-server.append_new_redel(nicki)
+async def startup(server):
+    # NEIL STARTING PROMPTS
+    # tbh this could be a for loop but I am lazy, so this is a Neil problem
+    nicki_system = (Path(__file__).parent / "pal_prompts/nicki_system.txt").read_text().strip()
+    nicki_info = (Path(__file__).parent / "pal_prompts/nicki_info.md").read_text().strip()
+    nicki = ReDel(
+        engine=engine,
+        system_prompt=nicki_system,
+        title="Nicki Martin, 46 F",
+        extra={"patient_info": nicki_info},  # see redel/utils.py for FrontendExtra
+    )
+    await server.append_new_redel(nicki)
 
-aaron_system = (Path(__file__).parent / "pal_prompts/aaron_system.txt").read_text().strip()
-aaron_info = (Path(__file__).parent / "pal_prompts/aaron_info.md").read_text().strip()
-aaron = ReDel(
-    engine=engine,
-    system_prompt=aaron_system,
-    title="Aaron Johnson, 55 M",
-    extra={"patient_info": aaron_info},
-)
-server.append_new_redel(nicki)
+    aiden_system = (Path(__file__).parent / "pal_prompts/aiden_system.txt").read_text().strip()
+    aiden_info = (Path(__file__).parent / "pal_prompts/aiden_info.md").read_text().strip()
+    aiden = ReDel(
+        engine=engine,
+        system_prompt=aiden_system,
+        title="Aiden Brown, 37 M",
+        extra={"patient_info": aiden_info},
+    )
+    await server.append_new_redel(aiden)
 
+    aaron_system = (Path(__file__).parent / "pal_prompts/aaron_system.txt").read_text().strip()
+    aaron_info = (Path(__file__).parent / "pal_prompts/aaron_info.md").read_text().strip()
+    aaron = ReDel(
+        engine=engine,
+        system_prompt=aaron_system,
+        title="Aaron Johnson, 55 M",
+        extra={"patient_info": aaron_info},
+    )
+    await server.append_new_redel(aaron)
+
+
+server = VizServer(engine=engine, startup_fn=startup)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
