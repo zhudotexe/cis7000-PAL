558d3f5e2335a84ac3241993bb549ac42672477c
Andrew Zhu <me@andrew-zhu.com>
2024-11-08 11:36:24 -0500

    chore: code style

--- pal-server.py
+++ pal-server.py
@@ -22,12 +22,14 @@ async def startup(server):
         engine=engine,
         system_prompt=nicki_system,
         title="Nicki Martin, 46 F",
-        extra={"patient_info": nicki_info, 
-               "patient_name": "Nicki Martin", 
-               "patient_age": "46",
-               "patient_gender": "F",
-               "patient_image_url": "/faces/nicki_martin.png", 
-               "patient_voice": "ug7mg45jVbzgYHpQBrw5"},  # see redel/utils.py for FrontendExtra
+        extra={
+            "patient_info": nicki_info,
+            "patient_name": "Nicki Martin",
+            "patient_age": "46",
+            "patient_gender": "F",
+            "patient_image_url": "/faces/nicki_martin.png",
+            "patient_voice": "ug7mg45jVbzgYHpQBrw5",
+        },  # see redel/utils.py for FrontendExtra
     )
     await server.append_new_redel(nicki)
 
@@ -37,12 +39,14 @@ async def startup(server):
         engine=engine,
         system_prompt=aiden_system,
         title="Aiden Brown, 37 M",
-        extra={"patient_info": aiden_info, 
-               "patient_name": "Aiden Brown", 
-               "patient_age": "37",
-               "patient_gender": "M",
-               "patient_image_url": "/faces/aiden_brown.png", 
-               "patient_voice": "Alex"},  # see redel/utils.py for FrontendExtra
+        extra={
+            "patient_info": aiden_info,
+            "patient_name": "Aiden Brown",
+            "patient_age": "37",
+            "patient_gender": "M",
+            "patient_image_url": "/faces/aiden_brown.png",
+            "patient_voice": "Alex",
+        },  # see redel/utils.py for FrontendExtra
     )
     await server.append_new_redel(aiden)
 
@@ -52,12 +56,14 @@ async def startup(server):
         engine=engine,
         system_prompt=aaron_system,
         title="Aaron Johnson, 55 M",
-        extra={"patient_info": aaron_info, "patient_name": 
-               "Aaron Johnson", 
-               "patient_age": "55",
-               "patient_gender": "M",
-               "patient_image_url": "/faces/aaron_johnson.png", 
-               "patient_voice": "Edward"},  # see redel/utils.py for FrontendExtra
+        extra={
+            "patient_info": aaron_info,
+            "patient_name": "Aaron Johnson",
+            "patient_age": "55",
+            "patient_gender": "M",
+            "patient_image_url": "/faces/aaron_johnson.png",
+            "patient_voice": "Edward",
+        },  # see redel/utils.py for FrontendExtra
     )
     await server.append_new_redel(aaron)
 
